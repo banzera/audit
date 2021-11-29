06af67ae1cbecd1500727cf0d8f7d4916de492a4
Bryan Banz <bryan@5and2studio.com>
2021-11-29 17:39:01 -0600

    Spike CodeReadr postback ingest

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -5,19 +5,49 @@ class ScansController < ActionController::Base
   def post
     @scan = InventoryScanOperation.new
 
-    # scan.user_id    = lookup_scan_user
-    @scan.sku_id      = scan_params['Scan Value']
-    @scan.dc_loc      = scan_params['DC Location']
-    @scan.scanned_at  = scan_params['']
-    @scan.received_at = scan_params['']
+    @scan.user_id = lookup_scan_user
+    @scan.dc_loc  = dc_loc
 
-    @scan.properties  = scan_params
+    @scan.sku_id      = scan_params[:tid]
+    @scan.scanned_at  = scan_params[:scanned_at_utc]
+    @scan.received_at = scan_params[:received_at_utc]
+    @scan.payload     = scan_params
+    @scan.request     = request_params
 
-    @scan.save
+    if @scan.save
+      # CodeReadrService.process_scan(@scan)
+    else
+    end
   end
 
   def scan_params
-    params.permit!
+    params.require(:scan).permit([
+      :tid,
+      :timestamp,
+      :scanned_at_utc,
+      :received_at_utc,
+      :scanid,
+      :sid,
+      :text,
+      :udid,
+      :userid,
+      :deviceid,
+      :status,
+      :time_zone,
+      :capture_type,
+      :scan_updated,
+      :scan_dbb_record,
+      questions: [],
+      answers: [],
+    ])
+  end
+
+  def dc_loc
+    scan_params[:answers].values.first
+  end
+
+  def request_params
+    {}
   end
 end
 
