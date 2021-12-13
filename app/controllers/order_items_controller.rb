083249b1504ca0bf71d388f311fc8208ebdf430c
Bryan Banz <bryan@5and2studio.com>
2021-12-13 11:25:10 -0600

    Spike out Order Items

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,29 @@
+class OrderItemsController < ApplicationController
+  include Effective::CrudController
+
+  load_and_authorize_resource
+  before_action :load_resources, except: [:index]
+
+  button :split, 'Split'
+
+  def update
+    resource.update permitted_params
+    respond_to do |format|
+      format.html { redirect_to receive_purchase_order_path(resource.purchase_order) }
+      format.js { render 'receive' }
+    end
+  end
+
+  private
+
+  def load_resources
+    @order = @order_item.order
+    @sku   = @order_item.sku
+  end
+
+  def permitted_params
+    params.require(:order_item).permit([
+    ])
+  end
+
+end
