42ccea77e0f0011eae4ba1d7b0903a2c2cf8533c
Bryan Banz <bryan@5and2studio.com>
2021-12-29 15:15:07 -0600

    Set Date/DateTime format defaults for effective datatables

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,4 +1,4 @@
 Time::DATE_FORMATS[:yymmdd] = "%y%m%d"
 
-Date::DATE_FORMATS[:american] = "%m/%d/%Y"
-Date::DATE_FORMATS[:default]  = "%m/%d/%Y"
+Date::DATE_FORMATS[:american] = "%D"
+Date::DATE_FORMATS[:default]  = "%D"
