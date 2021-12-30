a6b1e7b3b598c3ace93a8577520f627b56f83d79
Bryan Banz <bryan@5and2studio.com>
2021-12-29 23:41:21 -0600

    Update SKU TSV trigger to include the skuid

--- db/legacy/ddl/triggers.sql
+++ db/legacy/ddl/triggers.sql
@@ -1,51 +1,40 @@
 
 ALTER TABLE tblsku ADD COLUMN tsv tsvector;
 
+CREATE OR REPLACE FUNCTION update_sku_tsvector() RETURNS trigger AS $$
+BEGIN
+  new.tsv :=
+    to_tsvector('pg_catalog.english', coalesce(new.skuid::text,           '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.sku::text,             '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.manf::text,            '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.itemno::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.skudesc::text,         '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.unitweight::text,      '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.skuminunitstype::text, '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.dcloc::text,           '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.vsku01::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.vsku02::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.vsku03::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.vsku04::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.vsku05::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.vsku06::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.vsku07::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.vsku08::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.vsku09::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.vsku10::text,          '')) ||
+    to_tsvector('pg_catalog.english', coalesce(new.skunotes::text,        ''))
+    ;
+  return new;
+END
+$$ LANGUAGE plpgsql;
+
 CREATE TRIGGER tsvectorupdate
-BEFORE INSERT OR UPDATE ON tblsku
-FOR EACH ROW EXECUTE FUNCTION
-  tsvector_update_trigger(tsv, 'pg_catalog.english',
-    SKU,
-    Manf,
-    ItemNo,
-    SKUDesc,
-    UnitWeight,
-    SKUMinUnitsType,
-    DCLoc,
-    VSKU01,
-    VSKU02,
-    VSKU03,
-    VSKU04,
-    VSKU05,
-    VSKU06,
-    VSKU07,
-    VSKU08,
-    VSKU09,
-    VSKU10,
-    SKUNotes
-    );
+    BEFORE INSERT OR UPDATE ON tblsku
+    FOR EACH ROW EXECUTE FUNCTION update_sku_tsvector();
 
-UPDATE tblsku SET tsv =
-    to_tsvector('pg_catalog.english', coalesce(SKU::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(Manf::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(ItemNo::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(SKUDesc::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(UnitWeight::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(SKUMinUnitsType::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(DCLoc::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(VSKU01::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(VSKU02::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(VSKU03::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(VSKU04::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(VSKU05::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(VSKU06::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(VSKU07::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(VSKU08::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(VSKU09::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(VSKU10::text, '')) ||
-    to_tsvector('pg_catalog.english', coalesce(SKUNotes::text, ''))
+-- update all rows to trigger an update to tsv field
+UPDATE tblsku SET tsv = NULL;
 
-;
 DROP VIEW frm_sku_subform1;
 \i db/legacy/queries/frm_sku_subform1.sql
 
