36ac34c43ce8fcabea6971ae9e1878f3d2b849e8
Bryan Banz <bryan@5and2studio.com>
2022-01-04 17:12:49 -0600

    Factor out ItemImportService
    - make a generic file import service for objects with “items”

---
+++ app/services/item_import_service.rb
@@ -0,0 +1,51 @@
+class ItemImportService
+
+  def self.for(resource)
+    new(resource)
+  end
+  # end
+  def initialize(resource)
+    @resource = resource
+  end
+
+  def import(uploaded_file)
+    @file = uploaded_file
+
+    begin
+      workbook = Roo::Spreadsheet.open @file.path
+    rescue ArgumentError => e
+      raise ArgumentError.new('Invalid file type')
+    end
+
+    worksheet = workbook.sheet(target_sheet)
+    begin
+      first_row = worksheet.row_with self.class::HEADER_MAP.values
+    rescue Roo::HeaderRowNotFoundError
+      raise ArgumentError.new("Couldn't locate header row in first sheet #{target_sheet} of workbook. Expecting fields: #{self.class::HEADER_MAP.values}")
+    end
+
+    last_row  = worksheet.last_row
+
+    @resource.transaction do
+      worksheet.parse(self.class::HEADER_MAP).each_with_index do |row, i|
+
+        raise ArgumentError.new(mismatched_error(row[self.class::KEY_FIELD], i)) if row[self.class::KEY_FIELD] != @resource.id
+
+        @resource.items.build(row).save validate: false
+
+      end
+    end
+
+    last_row - first_row
+  end
+
+  private
+
+  def target_sheet
+    self.class::TARGET_SHEET
+  end
+
+  def mismatched_error(key_field, row)
+    "Mismatched key field value found in file on line #{row}. Expected #{@resource.id}. Found #{key_field}"
+  end
+end
--- app/services/purchase_order_import_service.rb
+++ app/services/purchase_order_import_service.rb
@@ -1,5 +1,7 @@
-class PurchaseOrderImportService
+class PurchaseOrderImportService < ItemImportService
 
+  TARGET_SHEET = 0
+  KEY_FIELD    = :poid
   HEADER_MAP = {
     skuid:              'SKUID',
     poid:               'POID',
@@ -16,38 +18,4 @@ class PurchaseOrderImportService
     poorderrebatenotes: 'POOrderRebateNotes',
   }
 
-  def self.for(purchase_order)
-    new(purchase_order)
-  end
-
-  def initialize(purchase_order)
-    @purchase_order = purchase_order
-  end
-
-  def import(uploaded_file)
-    @file = uploaded_file
-
-    begin
-      workbook = Roo::Spreadsheet.open @file.path
-    rescue ArgumentError => e
-      raise ArgumentError.new('Invalid file type')
-    end
-
-    worksheet = workbook.sheet 0
-    first_row = worksheet.row_with HEADER_MAP.values
-    last_row  = worksheet.last_row
-
-    worksheet.parse(HEADER_MAP).each_with_index do |row, i|
-      raise ArgumentError.new(mismatched_error(row[:poid], i)) if row[:poid] != @purchase_order.id
-      @purchase_order.items.create(row)
-    end
-
-    last_row - first_row
-  end
-
-  private
-
-  def mismatched_error(poid, row)
-    "Mismatched POID found in file on line #{row}. Expected #{@purchase_order.id}. Found #{poid}"
-  end
 end
