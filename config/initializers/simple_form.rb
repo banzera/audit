c54a01bb6ec0509b3e0cb290847d73375e77d895
Bryan Banz <bryan@5and2studio.com>
2022-01-06 16:00:41 -0600

    Update SimpleForm configuration
    - Don’t add a class for valid form values
    - Update the marker for required fields

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -62,7 +62,7 @@ SimpleForm.setup do |config|
     b.optional :calculated
     b.optional :data_attributes
     b.use :label
-    b.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
+    b.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: ''
     b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
     b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
   end
@@ -154,7 +154,7 @@ SimpleForm.setup do |config|
     b.optional :readonly
     b.use :label, class: 'col-sm-3 col-form-label'
     b.wrapper :grid_wrapper, tag: 'div', class: 'col-sm-9' do |ba|
-      ba.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
+      ba.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: ''
       ba.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
       ba.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
     end
