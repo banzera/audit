619748e629eec0d19dd8f17c8c2918f732fb125e
Bryan Banz <bryan@5and2studio.com>
2022-01-21 08:50:43 -0600

    WIP on PurchaseOrder selector widget

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,11 +1,24 @@
 class PurchaseOrdersController < ApplicationController
   include DefaultCrudController
+  include Select2Searchable
 
   load_and_authorize_resource
 
   button :upload, false
   button :new_items, 'Add or Upload Items'
   button :receive, 'Receive', if: -> { resource.unfullfilled? }
+
+  def index
+    respond_to do |format|
+      format.html { super }
+      format.json {
+        # @purchase_orders = @purchase_orders.search(select2_search_term) if select2_search_term?
+
+        @purchase_orders = @purchase_orders.for_sku_id(params[:with_sku]) if params[:with_sku].present?
+        @purchase_orders = @purchase_orders.limit(10)
+      }
+    end
+  end
 
   def new_items
     redirect_to new_purchase_order_purchase_order_item_path(resource)
