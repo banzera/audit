fdf5053c6300cdf43f0dd51ff8ddc25a34cdb9e4
Bryan Banz <bryan@5and2studio.com>
2022-02-21 15:15:14 -0600

    Fix sorting in PreOrder Items datatable

--- app/datatables/pre_order_items_datatable.rb
+++ app/datatables/pre_order_items_datatable.rb
@@ -3,6 +3,7 @@ class PreOrderItemsDatatable < Effective::Datatable
   collection do
     pre_order = attributes[:pre_order]
     scope     = pre_order.present? ? pre_order.items : PreOrderItem.none
+    scope.order(preorderitemsid: :asc)
   end
 
   filters do
@@ -16,26 +17,20 @@ class PreOrderItemsDatatable < Effective::Datatable
     order :preorderitemsid, :asc
 
     col :preorderitemsid, visible: false
-
-    col :orderdate, as: :date
-    col :skuid1,          visible: true, partial: 'pre_order_items/dt/sku'
-    # col :preorderid,      visible: true
-    col :skuid2,          visible: true, partial: 'pre_order_items/dt/sku'
-
-    col :poid,            visible: true, partial: 'application/dt/po'
-    col :orderdate, as: :date
-
+    col :orderdate, as: :date, label: 'Date'
+    col :skuid1,          label: 'SKU 1', visible: true, partial: 'pre_order_items/dt/sku', sort: false
+    col :skuid2,          label: 'SKU 2', visible: true, partial: 'pre_order_items/dt/sku', sort: false
+    col :poid,            label: 'POID',  visible: true, partial: 'application/dt/po',      sort: false
     col :code do |item| item.code&.preordercode end
-
-    col :orderquant1
-    col :orderquant2
-    col :orderpriceper1,   as: :currency
-    col :orderpriceper2,   as: :currency
-    col :orderpricetotal1, as: :currency
-    col :orderpricetotal2, as: :currency
-    col :orderaupriceper,  as: :currency
+    col :orderquant1,      label: 'Qty 1'
+    col :orderquant2,      label: 'Qty 2'
+    col :orderpriceper1,   as: :currency, sort: false, label: 'Price @ 1'
+    col :orderpriceper2,   as: :currency, sort: false, label: 'Price @ 2'
+    col :orderpricetotal1, as: :currency, sort: false, label: 'Price Total 1'
+    col :orderpricetotal2, as: :currency, sort: false, label: 'Price Total 2'
+    col :orderaupriceper,  as: :currency, sort: false, label: 'AU Price @'
     col :preordernotes, visible: false
-    col :preorderurgent, partial: 'application/dt/boolean'
+    col :preorderurgent, partial: 'application/dt/boolean', label: 'Urgent', sort: false
 
     # TODO: no actions when viewing through a PreOrder
     actions_col actions_partial: 'pre_order_items/dt/actions'
