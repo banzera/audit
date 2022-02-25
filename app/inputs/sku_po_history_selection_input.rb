f5ee05fd622981e7aa1c294c6e1ab6fcb13e1707
Bryan Banz <bryan@5and2studio.com>
2022-02-25 15:24:10 -0600

    Make some improvements to the SKU/PO history selector

--- app/inputs/sku_po_history_selection_input.rb
+++ app/inputs/sku_po_history_selection_input.rb
@@ -1,15 +1,23 @@
 class SkuPoHistorySelectionInput < SimpleSelect2Input
 
   def default_value
-    skuid = options[:for_sku] || object.skuid
-    SkuPoHistory.for_po(object.poid).for_sku(skuid).first
+    @dv ||= SkuPoHistory.for_po(object.poid).for_sku(skuid).first
   end
 
   def default_value_text
     @default_value_text ||= '' if options[:no_default] || !default_value
-
     @default_value_text ||= begin
       template.render partial: 'sku_po_histories/select2/selection', locals: { po: default_value }
+    end
+  end
+
+  def skuid
+    @skuid ||= begin
+      for_sku = options[:for_sku]
+      skuid = case for_sku
+      when Integer then for_sku
+      when Symbol then object.send(for_sku) if object.respond_to?(for_sku)
+      end
     end
   end
 
@@ -18,8 +26,8 @@ class SkuPoHistorySelectionInput < SimpleSelect2Input
       p[:available_only] = true if options[:available_only]
     end
 
-    if sku = (options[:for_sku] || object.skuid)
-      @url = template.po_history_sku_path(sku, params)
+    if skuid
+      @url = template.po_history_sku_path(skuid, params)
     else
       # raise ArgumentError.new("#{self.class.name} requires 'for_sku' option or an object with a `skuid` attribute")
       @url = template.sku_po_histories_path(params)
