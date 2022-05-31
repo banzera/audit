65b5c4da6f481e8c25c0ce5a598db7a39554d2e6
Bryan Banz <bryan@5and2studio.com>
2022-05-31 16:00:56 -0500

    Fix currency input styling and sanitization
    FIXES https://trello.com/c/aznet93k

--- app/inputs/currency_input.rb
+++ app/inputs/currency_input.rb
@@ -1,12 +1,10 @@
-class CurrencyInput < SimpleForm::Inputs::StringInput
+class CurrencyInput < SimpleForm::Inputs::NumericInput
   def input(wrapper_options={})
     haml = Haml::Engine.new <<~STR
       .input-group
         .input-group-prepend
           %span.input-group-text $
         #{super}
-        .input-group-append
-          %span.input-group-text .00
     STR
 
     haml.render
