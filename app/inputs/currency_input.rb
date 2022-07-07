5fc3183017e8d8b3169e36fbfb3e6f9bfe7bd805
Bryan Banz <bryan@5and2studio.com>
2022-07-06 23:14:05 -0500

    Improve currency inputs

--- app/inputs/currency_input.rb
+++ app/inputs/currency_input.rb
@@ -1,5 +1,12 @@
 class CurrencyInput < SimpleForm::Inputs::NumericInput
+  include ActionView::Helpers::NumberHelper
+
   def input(wrapper_options={})
+    raw_value       = object.public_send attribute_name
+    formatted_value = number_with_precision(raw_value, precision: 2)
+
+    input_html_options.reverse_merge!(value: formatted_value)
+
     haml = Haml::Engine.new <<~STR
       .input-group
         .input-group-prepend
