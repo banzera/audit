31f530745d46759e5ff65447b83d414437840076
Bryan Banz <bryan@5and2studio.com>
2022-07-08 10:32:00 -0500

    Add required field validations on Customer and fix i18n string

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,32 @@ class Customer < ApplicationRecord
   self.table_name  = 'tblcustomer'
   self.primary_key = 'custid'
 
+  validates :custname, presence: true
+  validates :custbusinessname, presence: true
+  validates :custfirst, presence: true
+  validates :custlast, presence: true
+  # validates :custsal, presence: true
+  # validates :custtitle, presence: true
+  # validates :custaddress, presence: true
+  # validates :custcity, presence: true
+  # validates :custst, presence: true
+  # validates :custzip, presence: true
+  # validates :custphone, presence: true
+  # validates :custfax, presence: true
+  # validates :custemail, presence: true
+  # validates :custprimarycontact1, presence: true
+  # validates :custphone1, presence: true
+  # validates :custphonetype1, presence: true
+  # validates :custemail1, presence: true
+  # validates :custprimarycontact2, presence: true
+  # validates :custphone2, presence: true
+  # validates :custphonetype2, presence: true
+  # validates :custemail2, presence: true
+  validates :custtaxrate, presence: true
+  # validates :custnotes, presence: true
+  # validates :custdatecreated, presence: true
+  # validates :custdatemodified, presence: true
+
   def full_name
     "#{custsal} #{custfirst} #{custlast}"
   end
