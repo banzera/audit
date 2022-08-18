555cb4ad2ea101d031e9c37b6755ca93316135bc
Bryan Banz <bryan@5and2studio.com>
2022-08-18 09:56:34 -0500

    Add prices/totals to pre-order items list

--- app/datatables/pre_order_items_datatable.rb
+++ app/datatables/pre_order_items_datatable.rb
@@ -22,7 +22,9 @@ class PreOrderItemsDatatable < Effective::Datatable
     col :preorderitemsid, visible: false
     col :orderdate, as: :date, label: 'Date'
     col :skuid1,          label: 'SKU 1', visible: true, partial: 'pre_order_items/dt/sku', sort: false
+    col :orderpricetotal1,label: 'Price', as: :currency
     col :skuid2,          label: 'SKU 2', visible: true, partial: 'pre_order_items/dt/sku', sort: false
+    col :orderpricetotal2,label: 'Price', as: :currency
     col :poid,            label: 'POID',  visible: true, partial: 'application/dt/po',      sort: false
     col :code do |item| item.code&.preordercode end
     col :orderquant1,      label: 'Qty 1'
@@ -35,5 +37,13 @@ class PreOrderItemsDatatable < Effective::Datatable
 
     # TODO: no actions when viewing through a PreOrder
     actions_col actions_partial: 'pre_order_items/dt/actions'
+
+    aggregate :total do |values, column|
+      if [:skuid1, :skuid2].include? column[:name]
+        fa_icon 'arrow-right', right: true, text: 'Total'
+      elsif [:orderpricetotal1, :orderpricetotal2].include? column[:name]
+        number_to_currency(values.compact.sum)
+      end
+    end
   end
 end
