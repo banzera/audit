14c2cff928f6c614656c1edcea37089948b5134f
Bryan Banz <bryan@5and2studio.com>
2023-06-21 15:17:11 -0500

    Implement PO invoice total tracking on PO index page

--- app/datatables/purchase_orders_datatable.rb
+++ app/datatables/purchase_orders_datatable.rb
@@ -13,7 +13,18 @@ class PurchaseOrdersDatatable < Effective::Datatable
     scope :all
     scope :outstanding
 
+    scope :prev5, label: scope_label(5)
+    scope :prev4, label: scope_label(4)
+    scope :prev3, label: scope_label(3)
+    scope :prev2, label: scope_label(2)
+    scope :prev1, label: scope_label(1)
+    scope :current, label: scope_label(0), default: true
+
     filter :sku, '', placeholder: "Fuzzy SKU Search"
+  end
+
+  def scope_label(x)
+    I18n.l(Date.today - x.months, format: :month)
   end
 
   datatable do
@@ -22,8 +33,8 @@ class PurchaseOrdersDatatable < Effective::Datatable
     col :poid
     col :podate,          visible: true,  search: false, as: :date
     col :pobatch,         visible: false
-    col :supplier1,       visible: true,  search: false, action: :show
-    col :supplier2,       visible: true,  search: false, action: :show
+    col :supplier1,       visible: true,  search: { collection: Supplier.all },  action: :show
+    col :supplier2,       visible: true,  search: { collection: Supplier.all },  action: :show
     col :poshipdate,      visible: true,  search: false, as: :date
     col :poshiptype,      visible: false, search: { as: :select, collection: PurchaseOrder.ship_types }
     col :potrackingno,    visible: false, search: false
@@ -31,11 +42,21 @@ class PurchaseOrdersDatatable < Effective::Datatable
     col :podatepaid,      visible: false, search: false, as: :date
     col :pomethodpaid,    visible: false, search: { as: :select, collection: PurchaseOrder.paid_methods }
     col :popaymentno,     visible: false, search: false
-    col :poamountpaid,    visible: true,  search: false, as: :currency
+    col :poamountpaid,    visible: false, search: false, as: :currency
     col :ponotes,         visible: false, search: false
     col :posplrorderno,   visible: true,  search: { as: :string, fuzzy: true }
     col :pochecksentdate, visible: false, search: false, as: :date
+    col :invoice_tax,      visible: false, search: false, as: :currency
+    col :invoice_sh,       visible: false, search: false, as: :currency
+    col :invoice_subtotal, visible: false, search: false, as: :currency
+    col :invoice_total,    visible: true,  search: false, as: :currency
 
     actions_col only: [:show]
+
+    aggregate :total do |values, column|
+      if column[:name] == :invoice_total
+        number_to_currency(values.compact.sum)
+      end
+    end
   end
 end
--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -21,6 +21,20 @@ class PurchaseOrder < ApplicationRecord
     where(poid: items)
   }
 
+  scope :in_range,     -> (date_range) { where(podate: date_range) }
+
+  scope :current, -> { in_range(monthrange(0)) }
+  scope :prev1,   -> { in_range(monthrange(1)) }
+  scope :prev2,   -> { in_range(monthrange(2)) }
+  scope :prev3,   -> { in_range(monthrange(3)) }
+  scope :prev4,   -> { in_range(monthrange(4)) }
+  scope :prev5,   -> { in_range(monthrange(5)) }
+
+  def self.monthrange(x)
+    day = Date.today - x.month
+    range = day.beginning_of_month..day.end_of_month
+  end
+
   validates_presence_of :supplier1,
                         :supplier2,
                         :posplrorderno,
